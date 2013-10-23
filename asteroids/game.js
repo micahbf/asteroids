86234fb1257d42e7b5d9018531918b76fbf1cac4
Isaac and Micah <imurchie@gmail.com>
2013-10-23 13:38:57 -0700

    Initial Game class

---
+++ asteroids/game.js
@@ -0,0 +1,47 @@
+(function() {
+  var Asteroids = window.Asteroids = (window.Asteroids || {});
+
+  var Game = Asteroids.Game = function(ctx) {
+    this.ctx = ctx;
+    this.asteroids = this.initAsteroids(10);
+  }
+
+  Game.DIM_X = 1000;
+  Game.DIM_Y = 1000;
+
+  Game.prototype.initAsteroids(numAsteroids) {
+    var asteroids = []
+    for(var i = 0; i< numAsteroids; i++) {
+      asteroids.push(Asteroids.Asteroid.randomAsteroid(Game.DIM_X, Game.DIM_Y));
+    }
+
+    return asteroids;
+  }
+
+  Game.prototype.draw = function() {
+    this.ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y);
+
+    var that = this;
+    this.asteroids.forEach(function(asteroid){
+      asteroid.draw(that.ctx);
+    });
+  }
+
+  Game.prototype.move = function() {
+    this.asteroids.forEach(function(asteroid) {
+      asteroid.move();
+    });
+  }
+
+  Game.prototype.step = function() {
+    this.move();
+    this.draw();
+  }
+
+  Game.prototype.start = function() {
+    var that = this;
+    window.setInterval(function() {
+      that.step();
+    }, 3000);
+  }
+})();
